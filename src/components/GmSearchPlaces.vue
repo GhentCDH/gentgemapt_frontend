7e3d4bf60609e2cf952932208ea0abf20c6ffcc6
Frederic Lamsens <frederic.lamsens@ugent.be>
2021-02-15 21:22:49 +0100

    add thumbnail

--- src/components/GmSearchPlaces.vue
+++ src/components/GmSearchPlaces.vue
@@ -1,21 +1,30 @@
 <template>
-  <div>
-    <input v-model="text" type="search">
-    <button @click="search">Search</button>
-    <p/>
-
-    <div v-if="response.count" v-for="item in response.items" class="result">
-      <b>Title:</b> {{item.title}}<br/>
-      <b>ID:</b> {{item.id}}<br/>
-      <br clear="left">
+  <div class="place-search">
+    <div class="place-search-from">
+      <b-input-group>
+        <b-form-input v-model="text" placeholder="" type="search"></b-form-input>
+        <b-input-group-append>
+          <b-button @click="search">Search</b-button>
+        </b-input-group-append>
+      </b-input-group>
     </div>
+    <div class="place-search-results flex-grow-1">
+      <ul class="list-unstyled">
+        <b-media v-if="response.count" v-for="item in response.items" tag="li">
+          <template #aside>
+            <b-img :blank="!Boolean(item.thumbnail)" blank-color="#777" width="40" :src="item.thumbnail"></b-img>
+          </template>
+          <h5>{{item.title}}</h5>
+        </b-media>
+      </ul>
 
-    <div v-if="noResults">
-      Sorry, but no results were found.
-    </div>
+      <div v-if="noResults">
+        Sorry, but no results were found.
+      </div>
 
-    <div v-if="searching">
-      <i>Searching...</i>
+      <div v-if="searching">
+        <i>Searching...</i>
+      </div>
     </div>
   </div>
 </template>
@@ -50,5 +59,20 @@ export default {
 </script>
 
 <style scoped>
+.place-search {
+  display:flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
 
+  .place-search-form {
+
+  }
+
+  .place-search-results {
+    flex-grow: 1;
+    overflow-y: auto;
+  }
+
+}
 </style>
