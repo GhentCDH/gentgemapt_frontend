c34b19b5c98ee569ab907809f262852d594c99b3
Frederic Lamsens <frederic.lamsens@ugent.be>
2023-06-29 10:58:16 +0200

    added google analytics config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,55 +1,59 @@
 const webpack = require('webpack')
-const { merge } = require('webpack-merge')
+const {merge} = require('webpack-merge')
 const common = require('./webpack.common.js')
 const paths = require('./paths')
+
+// plugins
 const Dotenv = require("dotenv-webpack");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
 
 
 module.exports = merge(common, {
-  // Set the mode to development or production
-  mode: 'development',
+    // Set the mode to development or production
+    mode: 'development',
 
-  // Control how source maps are generated
-  devtool: 'inline-source-map',
+    // Control how source maps are generated
+    devtool: 'inline-source-map',
 
-  // Spin up a server for quick development
-  devServer: {
-    historyApiFallback: true,
-    static: paths.build,
-    open: false,
-    compress: true,
-    hot: true,
-    port: 8080,
-  },
+    // Spin up a server for quick development
+    devServer: {
+        historyApiFallback: true,
+        static: paths.build,
+        open: false,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
 
-  module: {
-    rules: [
-      // Styles: Inject CSS into the head with source maps
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          {loader: 'css-loader', options: {sourceMap: true, importLoaders: 1, modules: false }}, // css modules disabled
-          {loader: 'postcss-loader', options: {sourceMap: true}},
-          {loader: 'resolve-url-loader'},
-          // Compiles Sass to CSS
-          {loader: 'sass-loader', options: {sourceMap: true}},
-        ],
-      },
-    ]
-  },
+    module: {
+        rules: [
+            // Styles: Inject CSS into the head with source maps
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    {loader: 'css-loader', options: {sourceMap: true, importLoaders: 1, modules: false}}, // css modules disabled
+                    {loader: 'postcss-loader', options: {sourceMap: true}},
+                    {loader: 'resolve-url-loader'},
+                    // Compiles Sass to CSS
+                    {loader: 'sass-loader', options: {sourceMap: true}},
+                ],
+            },
+        ]
+    },
 
-  plugins: [
-    // Only update what has changed on hot reload
-    new webpack.HotModuleReplacementPlugin(),
+    plugins: [
+        // Only update what has changed on hot reload
+        new webpack.HotModuleReplacementPlugin(),
 
-    new Dotenv({
-      defaults: true
-    }),
+        // dotenv
+        new Dotenv({
+            defaults: true
+        }),
 
-    // new BundleAnalyzerPlugin(),
-  ],
+        // new BundleAnalyzerPlugin(),
+    ],
 })
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,66 +1,77 @@
 const paths = require('./paths')
-const { merge } = require('webpack-merge')
+const {merge} = require('webpack-merge')
 const common = require('./webpack.common.js')
 
+// plugins
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
 
+// config
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: false,
-  output: {
-    path: paths.build,
-    publicPath: '/',
-    filename: 'js/[name].[contenthash].bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-              sourceMap: false,
-              modules: false, // css modules disabled
-            },
-          },
-          {loader: 'postcss-loader', options: {sourceMap: false}},
-          {loader: 'resolve-url-loader'},
-          {loader: 'sass-loader', options: {sourceMap: true}},
-        ],
-      },
-    ],
-  },
-  plugins: [
-    // Extracts CSS into separate files
-    // Note: style-loader is for development, MiniCssExtractPlugin is for production
-    new MiniCssExtractPlugin({
-      filename: 'styles/[name].[contenthash].css',
-      chunkFilename: '[id].css',
-    }),
-
-    new Dotenv({
-        path: './.env.prod',
-    }),
-
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), '...'],
-    // Once your build outputs multiple chunks, this option will ensure they share the webpack runtime
-    // instead of having their own. This also helps with long-term caching, since the chunks will only
-    // change when actual code changes, not the webpack runtime.
-    runtimeChunk: {
-      name: 'runtime',
+    mode: 'production',
+    devtool: false,
+    output: {
+        path: paths.build,
+        publicPath: '/',
+        filename: 'js/[name].[contenthash].bundle.js',
     },
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                            sourceMap: false,
+                            modules: false, // css modules disabled
+                        },
+                    },
+                    {loader: 'postcss-loader', options: {sourceMap: false}},
+                    {loader: 'resolve-url-loader'},
+                    {loader: 'sass-loader', options: {sourceMap: true}},
+                ],
+            },
+        ],
+    },
+    plugins: [
+        // Extracts CSS into separate files
+        // Note: style-loader is for development, MiniCssExtractPlugin is for production
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].[contenthash].css',
+            chunkFilename: '[id].css',
+        }),
+
+        // dotenv
+        new Dotenv({
+            path: './.env.prod',
+        }),
+
+        // partials
+        new HtmlWebpackPartialsPlugin({
+            path: paths.src + '/partials/analytics.html',
+            location: 'head',
+            priority: 'high'
+        })
+
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin(), '...'],
+        // Once your build outputs multiple chunks, this option will ensure they share the webpack runtime
+        // instead of having their own. This also helps with long-term caching, since the chunks will only
+        // change when actual code changes, not the webpack runtime.
+        runtimeChunk: {
+            name: 'runtime',
+        },
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
 })
