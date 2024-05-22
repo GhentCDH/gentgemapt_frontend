0280fa85951177b6d223df593c7905fd4ebe8b7b
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-05-22 10:38:58 +0200

    update place service to support projects

--- src/js/services/PlaceService.js
+++ src/js/services/PlaceService.js
@@ -5,12 +5,20 @@ export default {
         const { data } = await axios.get(process.env.URL_API + '/api/gentgemapt/place/get/' + parseInt(id) );
         return data;
     },
-    async search(text) {
-        const { data } = await axios.get(process.env.URL_API + '/api/gentgemapt/place/search?text=' + encodeURIComponent(text));
+    async search(text, project_id = null) {
+        let endpoint = process.env.URL_API + '/api/gentgemapt/place/search?text=' + encodeURIComponent(text);
+        if (project_id) {
+            endpoint += '&project_id=' + parseInt(project_id);
+        }
+        const { data } = await axios.get(endpoint);
         return data;
     },
-    async list() {
-        const { data } = await axios.get(process.env.URL_API + '/api/gentgemapt/place/list');
+    async list(project_id = null) {
+        let endpoint = process.env.URL_API + '/api/gentgemapt/place/list'
+        if (project_id) {
+            endpoint += '?project_id=' + parseInt(project_id);
+        }
+        const { data } = await axios.get(endpoint);
         return data;
     }
 }
