8e481ce0f8c872a492e32419a20cff927598ed05
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-05-23 15:45:41 +0200

    MapService: add support for project

--- src/js/services/MapService.js
+++ src/js/services/MapService.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
+import layers from '../data/layers'
+
 export default {
-    async list() {
-        const { data } = await axios.get(process.env.URL_API + '/api/gentgemapt/map/list');
-        return data;
+    async list(project_id = null) {
+        return layers;
+        // let endpoint = process.env.URL_API + '/api/gentgemapt/map/list'
+        // if (project_id) {
+        //     endpoint += '?project_id=' + parseInt(project_id);
+        // }
+        // const { data } = await axios.get(endpoint);
+        // return data;
     }
 }
